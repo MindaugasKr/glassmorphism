10f73fd76da62761b0a0ab8609a6be5f556bb41d
Mindaugas <mindaugas.kriksciukas.jp@gmail.com>
2022-01-18 21:45:12 +0200

    Add dummy home page schema.

---
+++ sanity/schemas/documents/home.ts
@@ -0,0 +1,38 @@
+export const home = {
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'template',
+      title: 'Template',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      description:
+        'If not happy with what the system generated, you can hand-edit it here',
+    },
+    {
+      name: 'message',
+      title: 'Message',
+      type: 'string',
+    },
+  ],
+  preview: {
+    select: {
+      slug: 'slug',
+      template: 'template',
+      message: 'message',
+    },
+    prepare(selection) {
+      const { slug, template, message } = selection;
+      return {
+        title: `${slug.current} [${template}]`,
+        subtitle: message,
+      };
+    },
+  },
+};
