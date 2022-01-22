9d6088870b364532090eedd30a80b4e9ac960064
Mindaugas <mindaugas.kriksciukas.jp@gmail.com>
2022-01-22 20:19:02 +0200

    Add eslint and prettier to web.

--- packages/web/gatsby-config.js
+++ packages/web/gatsby-config.js
@@ -1,30 +1,41 @@
 module.exports = {
   siteMetadata: {
-    title: `glassmorphism`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'glassmorphism',
+    siteUrl: 'https://www.yourdomain.tld',
   },
-  plugins: [{
-    resolve: 'gatsby-source-sanity',
-    options: {
-      "projectId": "nscjb4p4",
-      "dataset": "production"
-    }
-  }, "gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-VP28FDRZ6X"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'nscjb4p4',
+        dataset: 'production',
+      },
     },
-    __key: "images"
-  }]
-};
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'G-VP28FDRZ6X',
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
+}
